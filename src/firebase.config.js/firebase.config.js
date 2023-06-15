b715c4409b3a276dbd95697ac8faed49c24a481e
MOHAMMAD IKHTIAR UDDIN <ikhtiarbabor@gmail.com>
2023-06-15 22:25:07 +0600

    dist file done

--- src/firebase.config.js/firebase.config.js
+++ src/firebase.config.js/firebase.config.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -10,7 +10,7 @@ const firebaseConfig = {
   projectId: import.meta.env.VITE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 // Initialize Firebase
